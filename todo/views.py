67f8212e4ae37fac4716f213e148f0548a6739a5
Anya-amaJINcode <daebakak4@gmail.com>
2025-07-05 19:34:02 +0530

    created csrf token and implemented functionality for adding a task

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["clean", "groceries", "weekend-reset"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New task")
+    # priority = forms.IntegerField(label = "Priority", min_value = 1, max_value = 5)
+
 # Create your views here.
 def index(request):
     return render(request, "todo/index.html", {
@@ -8,6 +14,18 @@ def index(request):
     })
 
 def addTasks(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return render(request, "todo/index.html", {
+                "tasks" : tasks
+            })
+        else:
+            return render(request, "todo/addTasks.html", {
+                "form" : form
+            })
     return render(request, "todo/addTasks.html", {
-        "tasks" : tasks
+        "form" : NewTaskForm()
     })
