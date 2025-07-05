b46860d00a219186dfb512997e9ab198b74407da
Anya-amaJINcode <daebakak4@gmail.com>
2025-07-05 15:34:53 +0530

    commit fixes from a wrong hard reset; previous commits changes reflect here - working

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+app_name = "todo"
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", views.index, name = "index"),
+    path("/addTasks", views.addTasks, name = "addTasks"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,8 @@ def index(request):
     return render(request, "todo/index.html", {
         "tasks" : tasks
     })
+
+def addTasks(request):
+    return render(request, "todo/addTasks.html", {
+        "tasks" : tasks
+    })
