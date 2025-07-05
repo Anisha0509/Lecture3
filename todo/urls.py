b8c92846bbdffff16f6f70114d7d7674bf1757e5
Anya-amaJINcode <daebakak4@gmail.com>
2025-07-05 15:48:58 +0530

    addTasks.html url fix

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "todo"
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("/addTasks", views.addTasks, name = "addTasks"),
+    path("addTasks", views.addTasks, name = "addTasks"),
 ]
