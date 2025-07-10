5cadb72a106f0f48c604d1ec07fbd6a887f8bc04
Anisha0509 <anishakatta9@gmail.com>
2025-07-10 10:04:53 +0530

    made it persistent by using sql migrations, but same tasks list is displayed to all users, no authentication - working

--- todo/views.py
+++ todo/views.py
@@ -3,22 +3,28 @@ from django.shortcuts import render
 from django import forms
 from django.urls import reverse
 
+from .models import Notepad
+
 class NewTaskForm(forms.Form):
     task = forms.CharField(label = "New task")
     # priority = forms.IntegerField(label = "Priority", min_value = 1, max_value = 5)
 
 # Create your views here.
 def index(request):
-    if "tasks" not in request.session:
-        request.session["tasks"] = []
-    return render(request, "todo/index.html", { "tasks" : request.session["tasks"] })
+    # if "tasks" not in request.session:
+    #     request.session["tasks"] = []
+    # return render(request, "todo/index.html", { "tasks" : request.session["tasks"] })
+    tasks = Notepad.objects.all()
+    return render(request, "todo/index.html", { "tasks" : tasks})
 
 def addTasks(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            request.session["tasks"] += [task]
+            #request.session["tasks"] += [task]
+            item = Notepad ( taskItem = task )
+            item.save()
             return HttpResponseRedirect(reverse("todo:index"))
         else:
             return render(request, "todo/addTasks.html", {
